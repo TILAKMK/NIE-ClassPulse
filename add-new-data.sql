4488a7b7925cfa6884cc12e217311acd9658a7a1
TILAKMK <tilakbhat098@gmail.com>
2026-05-04 22:49:53 +0530

    Initial commit

--- add-new-data.sql
+++ add-new-data.sql
@@ -26,15 +26,16 @@ values
 -- TUESDAY
 ('208','Tuesday','09:00','10:00','MLDAP','MCA-A','2nd'),
 ('208','Tuesday','10:00','11:00','SE','MCA-A','2nd'),
-('208','Tuesday','11:30','12:30','WAD','MCA-A','2nd'),
-('208','Tuesday','12:30','13:30','JAVA','MCA-A','2nd'),
+
+
 ('208','Tuesday','14:30','15:30','DSA','MCA-A','2nd'),
 ('208','Tuesday','15:30','16:30','JAVA','MCA-A','2nd'),
 
 -- WEDNESDAY
-('208','Wednesday','09:00','10:00','JAVA','MCA-A','2nd'),
+
 ('208','Wednesday','11:30','12:30','SE','MCA-A','2nd'),
-('208','Wednesday','12:30','13:30','WAD','MCA-A','2nd'),
+('208','Wednesday','12:30','13:30','DSA','MCA-A','2nd'),
+('208','Wednesday','14:30','15:30','WAD','MCA-A','2nd'),
 
 -- THURSDAY
 ('208','Thursday','09:00','10:00','WAD','MCA-A','2nd'),
@@ -45,7 +46,7 @@ values
 -- FRIDAY
 ('208','Friday','09:00','10:00','DSA','MCA-A','2nd'),
 ('208','Friday','10:00','11:00','JAVA','MCA-A','2nd'),
-('208','Friday','11:30','12:30','SE','MCA-A','2nd')
+('208','Friday','11:30','13:30','SE','MCA-A','2nd')
 
 ) as v(room_number,day,start_time,end_time,subject,section,semester)
 on c.room_number = v.room_number;
@@ -83,6 +84,8 @@ values
 
 -- FRIDAY
 ('209','Friday','11:30','12:30','SE','MCA-B','2nd')
+('209','Friday','12:30','13:30','DSA','MCA-B','2nd')
+('209','Friday','14:30','16:30','SE TUTORIAL','MCA-B','2nd')
 
 ) as v(room_number,day,start_time,end_time,subject,section,semester)
 on c.room_number = v.room_number;
